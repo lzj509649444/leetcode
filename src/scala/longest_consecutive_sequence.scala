57e2c80748884a87148f3bf8612bf375b68528c8
lzj <509649444@163.com>
2015-11-11 23:14:11 +0800

    longest question

---
+++ src/scala/longest_consecutive_sequence.scala
@@ -0,0 +1,18 @@
+package scala
+
+/**
+ * Created by lzj on 15-11-11.
+ */
+
+/*Longest Consecutive Sequence
+描述
+Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
+For example, Given [100, 4, 200, 1, 3, 2], The longest consecutive elements sequence is [1,
+2, 3, 4]. Return its length: 4.
+Your algorithm should run in O(n) complexity.*/
+
+//第一想法事Hash,怎么优化Hash方法
+
+object longest_consecutive_sequence {
+
+}
