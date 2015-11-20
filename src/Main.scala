b8f4a26172c587936e21966d6663be90e6f7f3b0
lzj509649444 <509649444@163.com>
2015-11-20 15:03:17 +0800

    btree level order traversal

--- src/Main.scala
+++ src/Main.scala
@@ -1,12 +1,9 @@
-import scala.tree.{postorder_traversal_btree,inorder_traversal_btree,preorder_traversal_btree}
-
+import tree.level_traversal_btree
 /**
  * Created by lzj on 15-11-9.
  */
 object Main {
   def main(args: Array[String]): Unit = {
-    postorder_traversal_btree.run()
-    inorder_traversal_btree.run()
-    preorder_traversal_btree.run()
+    level_traversal_btree.run()
   }
 }
---
+++ src/scala/tree/level_traversal_btree.scala
@@ -0,0 +1,52 @@
+package scala.tree
+
+import scala.ADT.{TreeNode, NullNode, Tree}
+
+/**
+ * Created by luozhenjun on 15/11/20.
+ */
+object level_traversal_btree {
+  def level_order[Int](tree: Tree[Int]): List[Int] = {
+    def run[Int](xs: List[Tree[Int]]): List[Int] = xs match {
+      case Nil => Nil
+      case NullNode :: tail => run(tail)
+      case TreeNode(v,left,right) :: tail =>
+        v :: run(tail :+ left :+ right)
+    }
+    run(List(tree))
+  }
+
+  def run(): Unit ={
+    println(level_order(NullNode))
+    println(level_order(TreeNode(1,NullNode,NullNode)))
+    println(level_order(TreeNode(1,NullNode,TreeNode(2,NullNode,NullNode))))
+    println(level_order(TreeNode(1,TreeNode(2,NullNode,NullNode),NullNode)))
+
+    println(level_order(TreeNode(
+      1,
+      NullNode,
+      TreeNode(
+        2,
+        NullNode,
+        TreeNode(3,NullNode,NullNode)
+      )
+    )))
+
+    println(level_order(TreeNode(
+      1,
+      TreeNode(
+        2,
+        TreeNode(4,
+          TreeNode(6,NullNode,NullNode),
+          TreeNode(7,NullNode,NullNode)
+        ),
+        TreeNode(5,NullNode,NullNode)
+      ),
+      TreeNode(
+        3,
+        TreeNode(8,NullNode,NullNode),
+        TreeNode(9,NullNode,NullNode)
+      )
+    )))
+  }
+}
