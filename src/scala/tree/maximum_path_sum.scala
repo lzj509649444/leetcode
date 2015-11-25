c9fc57ab219d148a6a111481eb8cbdf6efe44e89
lzj <509649444@163.com>
2015-11-25 21:47:08 +0800

    add problem

---
+++ src/scala/tree/maximum_path_sum.scala
@@ -0,0 +1,14 @@
+package scala.tree
+
+/**
+ * Created by lzj on 15-11-25.
+ */
+/*
+Given a binary tree, find the maximum path sum.
+The path may start and end at any node in the tree. For example: Given the below binary tree,
+*/
+//假设知道如何求解任意节点为root的的最大path sum
+//数组最大连续子序列和
+object maximum_path_sum {
+
+}
---
+++ src/scala/tree/path_sum_ii.scala
@@ -0,0 +1,18 @@
+package scala.tree
+
+/**
+ * Created by lzj on 15-11-25.
+ */
+
+/*Path Sum II
+描述
+Given a binary tree and a sum, find all root-to-leaf paths where each path’s sum equals the given sum.
+For example: Given the below binary tree and sum = 22,
+return
+[
+[5,4,11,2],
+[5,8,4,5]
+]*/
+
+object path_sum_ii {
+}
