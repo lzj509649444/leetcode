f6f6533e91a36c7e5f153a595bd81e03c2d5fc55
lzj <509649444@163.com>
2015-12-01 22:53:26 +0800

    construct bstree

--- src/Main.scala
+++ src/Main.scala
@@ -1,10 +1,12 @@
-import scala.tree.construct_btree_from_preorder_and_inorder
+import scala.tree.{construct_btree_from_preorder_and_inorder, construct_btree_from_inorder_and_postorder}
 
 /**
  * Created by lzj on 15-11-9.
  */
 object Main {
   def main(args: Array[String]): Unit = {
+    construct_btree_from_inorder_and_postorder.run()
+    println()
     construct_btree_from_preorder_and_inorder.run()
   }
 }
--- src/scala/tree/construct_btree_from_inorder_and_postorder.scala
+++ src/scala/tree/construct_btree_from_inorder_and_postorder.scala
@@ -1,8 +1,38 @@
 package scala.tree
 
+import scala.ADT.{TreeNode, NullNode, Tree}
+
 /**
  * Created by lzj on 15-12-1.
  */
 object construct_btree_from_inorder_and_postorder {
+
+  def construct(ins: List[Int], poxs: List[Int]): Tree[Int] = ins match {
+    case Nil => NullNode
+    case List(v) => TreeNode(v,NullNode,NullNode)
+    case _ =>
+      val (head :+ v) = poxs
+      val pivot = ins.indexOf(v)
+      val (left,right) = ins.splitAt(pivot)
+      TreeNode(
+        v,
+        construct(left,head.take(left.length)),
+        construct(right.tail,head.drop(left.length)))
+  }
+
+  def run(): Unit ={
+    println(construct(List(2,1,3),List(2,3,1)))
+    println(construct(List(1,2),List(2,1)))
+    println(construct(List(2,1),List(2,1)))
+    println(construct(List(4,2,5,1,6,3,7),List(4,5,2,6,7,3,1)))
+    TreeNode(
+      1,
+      TreeNode(
+        5,
+        TreeNode(4,NullNode,NullNode),
+        TreeNode(2,NullNode,NullNode)),
+      TreeNode(7,TreeNode(6,NullNode,NullNode),TreeNode(3,NullNode,NullNode)))
+
+  }
 
 }
